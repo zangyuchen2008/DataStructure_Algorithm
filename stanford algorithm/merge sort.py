195dc822e2a54618f49dc47c726ee4feb78a0460
Zang Yuchen <zangyuchen2012@163.com>
2020-07-27 16:44:25 +0800

    Update merge sort.py

--- stanford algorithm/merge sort.py
+++ stanford algorithm/merge sort.py
@@ -1,30 +1,29 @@
 import numpy as np
-a = list(np.random.rand(13,))
+arr = [1,5,3,90,100,33,76,11]
 def merge_sort(arr):
-    lentgh = len(arr)
-    left_len = lentgh // 2
-    left = arr[:left_len]
-    right = arr[left_len:]
-    if left_len == 1 and len(right) == 1:
+    length = len(arr)
+    left_half = length // 2
+    right_half = length - left_half
+    left = arr[ : left_half]
+    right = arr[left_half :]
+    result = []
+    if left_half == 1 and right_half == 1:
         pass
-    elif left_len == 1:
+    elif left_half == 1:
         right = merge_sort(right)
     else:
         left = merge_sort(left)
         right = merge_sort(right)
-    i = 0
-    j = 0
     result = []
-    for _ in range(len(arr)-1):
-        if left[i] < right[j]:
+    i= 0
+    j= 0
+    for _ in range(len(arr)):
+        if left[i] < right [j]:
             result.append(left[i])
             i += 1
-        elif left[i] > right[j]:
+            if i > len(left)-1: return result + right[j:]
+        else:
             result.append(right[j])
-            j += 1 
-        if i > len(left)-1: return  result + right[j:]
-        elif j > len((right))-1: return result + left[i:]      
-    return result
-print(merge_sort(a))
-
-
+            j += 1
+            if j > len(right)-1: return result + left[i:]
+print(merge_sort(arr))  
